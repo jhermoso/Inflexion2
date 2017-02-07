a2802e6eca3f2cea108d7fa95cf425ff30fb79a1
Javier Hermoso Blanco <jhermoso@pretor.es>
2017-02-07 01:25:18 +0100

    cleaning

--- 08-Framework Candidates/Inflexion.Framework.UX.Windows/MVVM/ViewModels/Regions/IWorkspaceViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Inflexion.Framework.UX.Windows.MVVM.ViewModels.Regions
-{
-    public interface IWorkspaceViewModel
-    {
-        string Title { get; }
-        bool IsBusy { get; set; }
-    }
-}
