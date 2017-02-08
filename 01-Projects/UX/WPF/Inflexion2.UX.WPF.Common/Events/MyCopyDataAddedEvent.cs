a05225a05d6a9b28d77aa28ece2710ee6d939a53
Javier Hermoso Blanco <jhermoso@pretor.es>
2017-02-08 22:05:13 +0100

    ux projects cleaning

--- 01-Projects/UX/WPF/Inflexion2.UX.WPF.Common/Events/MyCopyDataAddedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Inflexion2.UX.WPF.Common.Events
-{
-    /// <summary>
-    /// event to send data
-    /// </summary>
-    public class MyCopyDataAddedEvent : Microsoft.Practices.Prism.Events.CompositePresentationEvent<MyCopyData>
-    {
-    }
-}
--- 01-Projects/UX/WPF/Inflexion2.UX.WPF.org/MVVM/ViewModels/Regions/IWorkspaceViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Inflexion2.UX.WPF.MVVM.ViewModels.Regions
-{
-    public interface IWorkspaceViewModel
-    {
-        string Title { get; }
-        bool IsBusy { get; set; }
-    }
-}
